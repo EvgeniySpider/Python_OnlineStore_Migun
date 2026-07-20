fd9aae83b0f8c2df37c8c09e66236d0085ca2f81
EvgeniySpider <evgeniy.migun@mail.ru>
2026-07-20 14:17:28 +0200

    feat: установить django-admin-interface

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'users',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Authentication settings
 AUTH_USER_MODEL = 'users.User'
