894d40175c51006b401ea528f97b044e86f4231e
EvgeniySpider <evgeniy.migun@mail.ru>
2026-07-21 22:42:06 +0200

    feat(auth): добавить регистрацию пользователей и форму CustomUserCreationForm
    - Создать форму CustomUserCreationForm на базе UserCreationForm в users/forms.py
    - Реализовать представление SignUpView с ограничением доступа для авторизованных через UserPassesTestMixin
    - Создать шаблон регистрации register.html
    - Изолировать маршруты аутентификации под префиксом 'auth/' в core/urls.py

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from users.views import SignUpView
+
+urlpatterns = [
+    path('register/', SignUpView.as_view(), name='register'),
+    
+]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from goods.models import Product
+from users.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
 
 
 class IndexView(ListView):
     model = Product
     template_name = 'index.html'
     context_object_name = 'products'  # Имя переменной в шаблоне
+
+
+class SignUpView(UserPassesTestMixin, CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/register.html'
+
+    def test_func(self):
+        # Доступ разрешен ТОЛЬКО анонимным пользователям (неавторизованным)
+        return not self.request.user.is_authenticated
+
+    def handle_no_permission(self):
+        # Если пользователь УЖЕ залогинен — кидаем его на главную страницу
+        return redirect('home')
