9298d29612ff0d671ea71511b3168552d65ec9dd
EvgeniySpider <evgeniy.migun@mail.ru>
2026-07-21 20:37:37 +0200

    feat: добавить главную страницу с отображением каталога товаров
    - Создать шаблон index.html для отображения списка товаров
    - Реализовать IndexView на базе ListView для вывода продуктов
    - Настроить маршрут для главной страницы ('') в urls.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from goods.models import Product
+
+
+class IndexView(ListView):
+    model = Product
+    template_name = 'index.html'
+    context_object_name = 'products'  # Имя переменной в шаблоне
